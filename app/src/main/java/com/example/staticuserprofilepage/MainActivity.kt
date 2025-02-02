f25470f222060dd187d22c85b381f10993f76bdb
Sukie Zhang <sukiezhang.sz@gmail.com>
2025-02-02 17:15:21 -0500

    implemented snackbar to display messages of whether they are following the user or not

--- app/src/main/java/com/example/staticuserprofilepage/MainActivity.kt
+++ app/src/main/java/com/example/staticuserprofilepage/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.staticuserprofilepage.ui.theme.StaticUserProfilePageTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +35,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StaticUserProfilePageTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val snackbarHostState = remember { SnackbarHostState() }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+                ) { innerPadding ->
+                    UserProfile(
+                        modifier = Modifier
+                            .padding(innerPadding),
+                        snackbarHostState = snackbarHostState
                     )
                 }
             }
@@ -31,17 +53,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UserProfile(modifier: Modifier = Modifier, snackbarHostState: SnackbarHostState) {
+    val isFollowing = remember { mutableStateOf(false) }
+
+    val snackbarMessage = if (isFollowing.value) {
+        "You are now following the user."
+    } else {
+        "You have unfollowed the user."
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+    ) {
+        Image( // profile picture default avatar
+            painter = painterResource(R.drawable.avatar),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(150.dp)
+                .clip(CircleShape)
+        )
+
+        Column(
+            modifier = modifier
+                .padding(20.dp)
+        ) {
+            Text(
+                text = "username",
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = "insert biography"
+            )
+
+            Button(onClick = {
+                isFollowing.value = !isFollowing.value
+            }) {
+                Text(text = if (isFollowing.value) "Following" else "Follow")
+            }
+
+            LaunchedEffect(isFollowing.value) {
+                snackbarHostState.showSnackbar(snackbarMessage)
+            }
+        }
+    }
+    LaunchedEffect(snackbarHostState) {
+        snackbarHostState.showSnackbar(snackbarMessage)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun UserProfilePreview() {
+    val snackbarHostState = remember { SnackbarHostState() }
     StaticUserProfilePageTheme {
-        Greeting("Android")
+        UserProfile(snackbarHostState = snackbarHostState)
     }
 }
